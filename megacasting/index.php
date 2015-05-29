49dfb17130d29fd6fcd9889a36b38e33475732ac
Lepage <alexislepage@hotmail.fr>
2015-05-29 17:11:57 +0200

    WebSite Megacasting V2.3.0
    Vendredi soir !

--- megacasting/index.php
+++ megacasting/index.php
@@ -156,19 +156,18 @@
     <section id="post">
         <div class="container_post">
             <?php
-              $resultats=$bdd->query("SELECT desc_offre, int_offre, date_offre, nom_annonceur, lib_domaine FROM offre INNER JOIN domaine ON domaine.id_domaine=offre.id_domaine INNER JOIN annonceur ON annonceur.id_annonceur=offre.id_annonceur");
+              $resultats=$bdd->query("SELECT id_offre, desc_offre, int_offre, date_offre, nom_annonceur, lib_domaine FROM offre INNER JOIN domaine ON domaine.id_domaine=offre.id_domaine INNER JOIN annonceur ON annonceur.id_annonceur=offre.id_annonceur");
               $resultats->setFetchMode(PDO::FETCH_OBJ);
               while( $resultat = $resultats->fetch() )
               {
-                echo "<div class='container_annonce'><header class='post_annonce'><span class='post_dom'>";
+                echo "<a href='personnal.php?id_offre=" . $resultat->id_offre . "'><div class='container_annonce'><header class='post_annonce'><span class='post_dom'>";
                 if($resultat->lib_domaine == 'Chant'){ echo "<img src='images/domaine/chant.jpg'"; } else if($resultat->lib_domaine == 'Musicien'){ echo "<img src='images/domaine/music.jpg'"; } else { echo "<img src='images/domaine/notfound.png'"; }
-                echo "</span><span id='post_title'>\"".$resultat->int_offre."\"</span><span id='post_dom'></span><span id='post_by'>Posté par ".$resultat->nom_annonceur."</span><span id='post_date'>".$resultat->date_offre."</span></header><article>".$resultat->desc_offre."</article></div>";
+                echo "</span><span id='post_title'>\"".$resultat->int_offre."\"</span><span id='post_dom'></span><span id='post_by'>Posté par ".$resultat->nom_annonceur."</span><span id='post_date'>".$resultat->date_offre."</span></header><article>".$resultat->desc_offre."</article></div></a>";
               }
-              $resultats->closeCursor();
+              $resultats->closeCursor();           
             ?>
         </div>
     </section><!--/#middle-->
-
     </br>
     <section id="conatcat-info">
         <div class="container">
